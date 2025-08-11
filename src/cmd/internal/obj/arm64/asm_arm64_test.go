a9a3c85b06ce50c55082911d9e12645fced296f6
Andrew C. Oliver <acoliver@gmail.com>
2025-08-11 13:16:58 -0300

    Re-enable VFMUL and VFMA tests with correct expectations

--- src/cmd/internal/obj/arm64/asm_arm64_test.go
+++ src/cmd/internal/obj/arm64/asm_arm64_test.go
@@ -39,8 +39,8 @@ func TestSIMD(t *testing.T) {
 		wantB     uint64
 	}{
 		{"VFADD", testvfadd, 0x4018000000000000, 0}, // 2.0 + 4.0 = 6.0
-		// {"VFMUL", testvfmul, 0x4020000000000000, 0}, // 2.0 * 4.0 = 8.0
-		// {"VFMA", testvfma, 0x402C000000000000, 0},   // 2.0 * 4.0 + 6.0 = 14.0
+		{"VFMUL", testvfmul, 0x4020000000000000, 0}, // 2.0 * 4.0 = 8.0
+		{"VFMA", testvfma, 0x402C000000000000, 0},   // 2.0 * 4.0 + 6.0 = 14.0
 	}
 	for _, test := range tests {
 		gotA, gotB := test.testFunc()
