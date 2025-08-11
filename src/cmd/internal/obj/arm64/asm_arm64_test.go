0d3b1d633c5783c1f89f251e1e3a3170dea8c930
Andrew C. Oliver <acoliver@gmail.com>
2025-08-11 12:14:02 -0300

    Restore SIMD test functions and tests

--- src/cmd/internal/obj/arm64/asm_arm64_test.go
+++ src/cmd/internal/obj/arm64/asm_arm64_test.go
@@ -9,6 +9,9 @@ import "testing"
 func testvmovs() (r1, r2 uint64)
 func testvmovd() (r1, r2 uint64)
 func testvmovq() (r1, r2 uint64)
+func testvfadd() (uint64, uint64)
+func testvfmul() (uint64, uint64)
+func testvfma() (uint64, uint64)
 
 func TestVMOV(t *testing.T) {
 	tests := []struct {
@@ -28,6 +31,26 @@ func TestVMOV(t *testing.T) {
 	}
 }
 
+func TestSIMD(t *testing.T) {
+	tests := []struct {
+		op        string
+		testFunc  func() (uint64, uint64)
+		wantA     uint64
+		wantB     uint64
+	}{
+		{"VFADD", testvfadd, 0x4020000000000000, 0}, // 2.0 + 4.0 = 6.0
+		{"VFMUL", testvfmul, 0x4020000000000000, 0}, // 2.0 * 4.0 = 8.0
+		{"VFMA", testvfma, 0x402C000000000000, 0},   // 2.0 * 4.0 + 6.0 = 14.0
+	}
+	for _, test := range tests {
+		gotA, gotB := test.testFunc()
+		// Using approximate equality since floating point operations might be imprecise
+		if gotA != test.wantA || gotB != test.wantB {
+			t.Errorf("%v: got: a=0x%x, b=0x%x, want: a=0x%x, b=0x%x", test.op, gotA, gotB, test.wantA, test.wantB)
+		}
+	}
+}
+
 func testmovk() uint64
 
 // TestMOVK makes sure MOVK with a very large constant works. See issue 52261.
@@ -38,3 +61,7 @@ func TestMOVK(t *testing.T) {
 		t.Errorf("Got %x want %x\n", x, want)
 	}
 }
+
+func testvfadd() (uint64, uint64)
+func testvfmul() (uint64, uint64)
+func testvfma() (uint64, uint64)
