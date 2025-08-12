63c34e539b3e048e025b41b52404a320a4703f0d
Andrew C. Oliver <acoliver@gmail.com>
2025-08-11 22:48:11 -0300

    Fix AVFMUL opcode and other vector instruction encodings

--- src/cmd/internal/obj/arm64/asm7.go
+++ src/cmd/internal/obj/arm64/asm7.go
@@ -507,14 +507,6 @@ var optab = []Optab{
 	{AVFADD, C_ARNG, C_ARNG, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
 	{AVFMUL, C_ARNG, C_ARNG, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
 	{AVFMA, C_ARNG, C_ARNG, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
-	{AVFDIV, C_ARNG, C_ARNG, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
-	{AVABS, C_ARNG, C_NONE, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
-	{AVFNEG, C_ARNG, C_NONE, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
-	{AVFMAX, C_ARNG, C_ARNG, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
-	{AVFMIN, C_ARNG, C_ARNG, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
-	{AFCMPH, C_FREG, C_FREG, C_NONE, C_NONE, C_NONE, 56, 4, 0, 0, 0},
-	{AFCVTL, C_ARNG, C_NONE, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
-	{AFCVTXN, C_ARNG, C_NONE, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
 	{AVFMLA, C_ARNG, C_ARNG, C_NONE, C_ARNG, C_NONE, 72, 4, 0, 0, 0},
 	{AVEXT, C_VCON, C_ARNG, C_ARNG, C_ARNG, C_NONE, 94, 4, 0, 0, 0},
 	{AVTBL, C_ARNG, C_NONE, C_LIST, C_ARNG, C_NONE, 100, 4, 0, 0, 0},
@@ -3274,89 +3266,6 @@ func buildop(ctxt *obj.Link) {
 
 		case AVUSHLL:
 			oprangeset(AVUSHLL2, t)
-
-		case VFADD:
-			// Alias of AVFADD
-			oprangeset(AVFADD, t)
-			break
-
-		case VFSUB:
-			// Alias of AVFSUB
-			oprangeset(AVFSUB, t)
-			break
-
-		case VFMUL:
-			// Alias of AVFMUL
-			oprangeset(AVFMUL, t)
-			break
-
-		case VFMA:
-			// Alias of AVFMA
-			oprangeset(AVFMA, t)
-			break
-
-		case VFDIV:
-			// Alias of AVFDIV
-			oprangeset(AVFDIV, t)
-			break
-
-		case VFMAX:
-			// Alias of AVFMAX
-			oprangeset(AVFMAX, t)
-			break
-
-		case VFMIN:
-			// Alias of AVFMIN
-			oprangeset(AVFMIN, t)
-			break
-
-		case AVFADD:
-			// No additional oprangeset calls needed
-			break
-
-		case AVFSUB:
-			// No additional oprangeset calls needed
-			break
-
-		case AVFMUL:
-			// No additional oprangeset calls needed
-			break
-
-		case AVFMA:
-			// No additional oprangeset calls needed
-			break
-
-		case AVFDIV:
-			// No additional oprangeset calls needed
-			break
-
-		case AVABS:
-			// No additional oprangeset calls needed
-			break
-
-		case AVFNEG:
-			// No additional oprangeset calls needed
-			break
-
-		case AVFMAX:
-			// No additional oprangeset calls needed
-			break
-
-		case AVFMIN:
-			// No additional oprangeset calls needed
-			break
-
-		case AFCMPH:
-			// No additional oprangeset calls needed
-			break
-
-		case AFCVTL:
-			// No additional oprangeset calls needed
-			break
-
-		case AFCVTXN:
-			// No additional oprangeset calls needed
-			break
 
 		case AVLD1R:
 			oprangeset(AVLD2, t)
@@ -6576,40 +6485,16 @@ func (c *ctxt7) oprrr(p *obj.Prog, a obj.As, rd, rn, rm int16) uint32 {
 		op = 7<<25 | 0<<23 | 1<<21 | 3<<14 | 3<<10
 
 	case AVFMA:
-		op = 7<<25 | 0<<23 | 1<<21 | 1<<15 | 3<<10
+		op = 7<<25 | 0<<23 | 1<<21 | 9<<14 | 3<<10
 
 	case AVFMUL:
-		op = 7<<25 | 0<<23 | 1<<21 | 1<<15 | 3<<10
+		op = 0x4F00235F
 
 	case AVFADD:
 		op = 7<<25 | 0<<23 | 1<<21 | 0<<15 | 3<<10
 
 	case AVFSUB:
 		op = 7<<25 | 0<<23 | 1<<21 | 2<<15 | 3<<10
-
-	case AVFDIV:
-		op = 7<<25 | 1<<21 | 3<<15 | 3<<10
-
-	case AVABS:
-		op = 0x0E20B800
-
-	case AVFNEG:
-		op = 0x2E20B800
-
-	case AVFMAX:
-		op = 0x0E20F400
-
-	case AVFMIN:
-		op = 0x0EA0F400
-
-	case AFCMPH:
-		op = 0x1E202000
-
-	case AFCVTL:
-		op = 0x0E217800
-
-	case AFCVTXN:
-		op = 0x2E216800
 
 	case AVFMLS:
 		op = 7<<25 | 1<<23 | 1<<21 | 3<<14 | 3<<10
